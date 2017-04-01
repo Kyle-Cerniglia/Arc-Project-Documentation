cf0be2094f5cda6969a8ac371f0c7389f9c0c1fb
Netan-MalDoran <kylecerniglia@email.arizona.edu>
2017-03-31 18:29:05 -0700

    Add files via upload

---
+++ Motor Test Board/Program/Program.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial output(20, 1);
+
+int address = 1;
+int directionv = 1;
+int speedv = 0;
+  
+void setup() {
+  output.begin(300);
+}
+
+void loop() {
+  speedv = analogRead(3);
+  speedv = map(speedv, 0, 1023, 0, 255);
+  
+  output.write(address);
+  delay(10);
+  output.write(directionv);
+  delay(10);
+  output.write(speedv);
+  delay(10);
+}
+  
+
---
+++ Motor Test Board/V1.0.0/V1.0.0.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial output(20, 1);
+
+int speedi = 0;
+  
+void setup() {
+  output.begin(300);
+}
+
+void loop() {
+  speedi = analogRead(3);
+  speedi = map(speedi, 0, 1023, 0, 255);
+  output.write(speedi);
+}
